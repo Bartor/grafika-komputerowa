4ce614fa33cad5a9382b6af9f061f485c2e2b600
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-10-30 10:47:10 +0100

    wyczyszczono strukturę kodu

--- lista1/shared/WireFrame.js
+++ lista1/shared/WireFrame.js
@@ -4,7 +4,14 @@ class WireFrame {
         this.dHeight = canvasElement.height / 2;
         this.dWidth = canvasElement.width / 2;
 
+        this.shapes = [];
+        this.points = [];
         this.perspective = perspective;
+    }
+
+    addShapes(...shapes) {
+        this.shapes = [...this.shapes, ...shapes];
+        this.points = [...this.points, ...shapes.reduce((acc, shape) => [...acc, ...shape.toMove()], [])];
     }
 
     render(renderList) {
@@ -14,5 +21,20 @@ class WireFrame {
             this.context.lineTo(renderPoint.end.x + this.dWidth, renderPoint.end.y + this.dHeight);
         });
         this.context.stroke();
+    }
+
+    draw() {
+        this.context.clearRect(0, 0, this.dHeight * 2 + 1, this.dHeight * 2 + 1);
+        this.shapes.forEach(shape => {
+            this.render(shape.toLines().reduce((acc, line) => [...acc, ...line.project(this.perspective)], []));
+        });
+    }
+
+    move(dx = 0, dy = 0, dz = 0) {
+        this.points.forEach(point => {
+            point.x += dx;
+            point.y += dy;
+            point.z += dz;
+        })
     }
 }
--- lista1/zad4/wireFramePage.js
+++ lista1/zad4/wireFramePage.js
@@ -4,48 +4,24 @@ window.addEventListener('load', () => {
     const canvas = document.getElementById('wireFrameCanvas');
     const wireFrame = new WireFrame(canvas, perspective);
 
-    let shapes = [
+    wireFrame.addShapes(
         new Cuboid({x: -100, y: -100, z: 100}, {x: 100, y: 100, z: 200}),
-        new Cuboid({x: 100, y: 100, z: 100}, {x: 200, y: 200, z: 400}),
-    ];
+        new Cuboid({x: 100, y: 100, z: 100}, {x: 200, y: 200, z: 400})
+    );
 
-    let rerender = timestamp => {
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-        shapes.forEach(shape => wireFrame.render(shape.toLines().reduce((acc, line) => [...acc, ...line.project(200)], [])));
+    let rerender = () => {
+        wireFrame.draw();
         window.requestAnimationFrame(rerender);
     };
-
     window.requestAnimationFrame(rerender);
 
-    const points = shapes.reduce((acc, shape) => [...acc, ...shape.toMove()], []);
     let drag = false;
     canvas.addEventListener('mousedown', () => drag = true);
     canvas.addEventListener('mouseup', () => drag = false);
     canvas.addEventListener('mouseout', () => drag = false);
     canvas.addEventListener('mousemove', event => {
         if (drag) {
-            points.forEach(point => {
-                point.x += event.movementX;
-                point.y += event.movementY;
-            });
-        }
-    });
-
-    window.addEventListener('keydown', event => {
-        const delta = 10;
-        switch (event.key) {
-            case 'ArrowLeft':
-                points.forEach(point => point.x -= delta);
-                break;
-            case 'ArrowRight':
-                points.forEach(point => point.x += delta);
-                break;
-            case 'ArrowDown':
-                points.forEach(point => point.z += delta);
-                break;
-            case 'ArrowUp':
-                points.forEach(point => point.z -= delta);
-                break;
+            wireFrame.move(event.movementX, event.movementY);
         }
     });
 });
